4afa80ff2304f36cc951a5086ecc7eecc67cfcf1
amadou <amadou.balde.2023@student.lhschools.org>
2020-03-04 07:57:09 -0500

    OddnessMethodCreated

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,22 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        testOddness();
+
+
     }
-}
+        public static void testOddness(){
+
+             int number = 17;
+
+            Scanner input;
+            input= new Scanner(System.in);
+
+            System.out.println("Number is odd:"+(number % 2 != 0));
+        }
+    }
+
