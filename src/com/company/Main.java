78b0569a7984abb0cd42e4b1022ea152d90af0aa
amadou <amadou.balde.2023@student.lhschools.org>
2020-03-05 13:27:12 -0500

    LastDigitMethodCreated

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         testOddness();
         KeepContained();
-
+        lastdigit();
 
     }
         public static void testOddness() {
@@ -25,15 +25,26 @@ public class Main {
 
         }
 
-            public static void KeepContained()  {
+            public static void KeepContained() {
 
                 int number;
 
                 Scanner keyboard;
-               keyboard = new Scanner(System.in);
+                keyboard = new Scanner(System.in);
                 System.out.println("Give me a positive number");
                 number = keyboard.nextInt();
                 System.out.println("number contained is:" + (number % 8));
+            }
+
+
+                public static void lastdigit()  {
+
+                int number;
+                Scanner keyboard;
+                keyboard = new Scanner(System.in);
+                System.out.println("Give me a positive number that is 4 digits long");
+                number = keyboard.nextInt();
+                System.out.println("Last digit of number is:" + number%10);
 
 
 
